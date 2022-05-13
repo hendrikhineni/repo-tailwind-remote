1e6c0f5d131ba454a4a4632ac8ff62d9fbad9d7f
Hendry P <hendryprasetyo1508@gmail.com config --global user.name Hendry>
2022-05-14 04:22:42 +0700

    remote portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors : {
+        primary : '#22d3ee',
+        dark : '#0f172a',
+      },
+      },
+    },
+  plugins: [],
+}
